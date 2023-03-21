71c229fcc8e906740af17cbce6055a2da705f2de
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-21 22:13:09 +0000

    Fixed linter errors

--- src/components/CalculatorScreen.jsx
+++ src/components/CalculatorScreen.jsx
@@ -1,4 +1,4 @@
 function CalculatorScreen() {
-    return <div className=" rounded-sm h-10 w-[20%] bg-slate-400 text-right text-white p-2">0</div>
+  return <div className=" rounded-sm h-10 w-[20%] bg-slate-400 text-right text-white p-2">0</div>;
 }
-export default CalculatorScreen
+export default CalculatorScreen;
