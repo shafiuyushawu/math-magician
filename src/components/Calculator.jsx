31c61b86864743bc815efb6ce6a62f3b42672839
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-22 22:07:35 +0000

    Implemented events to calculate input figures using operands

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,38 +1,50 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 import CalculatorScreen from './CalculatorScreen';
 
 function Calculator() {
+  const [objState, setObjState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleClick = (btnButton) => {
+    const newObjState = calculate(objState, btnButton);
+    setObjState(newObjState);
+  };
+
   return (
     <>
       <div className="text-xl flex flex-col justify-center p-10">
-        <CalculatorScreen />
+        <CalculatorScreen objState={objState} />
         <div>
-          <div className="btn text-xl w-[5%] bg-slate-200">AC</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">+/-</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">%</div>
-          <div className="btn text-xl w-[5%] bg-orange-400">/</div>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('AC')}>AC</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('+/-')}>+/-</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('%')}>%</button>
+          <button type="button" className="btn text-xl w-[5%] bg-orange-400" onClick={() => handleClick('÷')}>÷</button>
         </div>
         <div>
-          <div className="btn text-xl w-[5%] bg-slate-200">7</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">8</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">9</div>
-          <div className="btn text-xl w-[5%] bg-orange-400">X</div>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('7')}>7</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('8')}>8</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('9')}>9</button>
+          <button type="button" className="btn text-xl w-[5%] bg-orange-400" onClick={() => handleClick('x')}>x</button>
         </div>
         <div>
-          <div className="btn text-xl w-[5%] bg-slate-200">4</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">5</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">6</div>
-          <div className="btn text-xl w-[5%] bg-orange-400">-</div>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('4')}>4</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('5')}>5</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('6')}>6</button>
+          <button type="button" className="btn text-xl w-[5%] bg-orange-400" onClick={() => handleClick('-')}>-</button>
         </div>
         <div>
-          <div className="btn text-xl w-[5%] bg-slate-200">1</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">2</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">3</div>
-          <div className="btn text-xl w-[5%] bg-orange-400">+</div>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('1')}>1</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('2')}>2</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('3')}>3</button>
+          <button type="button" className="btn text-xl w-[5%] bg-orange-400" onClick={() => handleClick('+')}>+</button>
         </div>
         <div>
-          <div className="btn text-xl w-[10%] bg-slate-200">0</div>
-          <div className="btn text-xl w-[5%] bg-slate-200">.</div>
-          <div className="btn text-xl w-[5%] bg-orange-400">=</div>
+          <button type="button" className="btn text-xl w-[10%] bg-slate-200" onClick={() => handleClick('0')}>0</button>
+          <button type="button" className="btn text-xl w-[5%] bg-slate-200" onClick={() => handleClick('.')}>.</button>
+          <button type="button" className="btn text-xl w-[5%] bg-orange-400" onClick={() => handleClick('=')}>=</button>
         </div>
       </div>
     </>
